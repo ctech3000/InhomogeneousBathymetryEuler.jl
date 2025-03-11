006166cfb8192f98769b3645296eb81c15dc55ee
Christoph Zetek <christoph.zetek@tuhh.de>
2025-03-11 10:02:38 +0100

    asdf

--- examples/Plots/eulerConvRegPlots.jl
+++ examples/Plots/eulerConvRegPlots.jl
@@ -1,13 +1,12 @@
 using InhomogeneousBathymetryEuler, GLMakie, JLD2
 
-pwd()
 inhom = false
 true_eta_ana = false
 
-if inhom
+#= if inhom
     filename = "examples/Plots/convergenceDataRegEulerInhom.jld2"
 else
-    filename = "examples/Plots/convergenceDataRegEuler.jld2"
+    filename = "convergenceDataRegEuler.jld2"
 end
 d = load(filename)
 
@@ -19,6 +18,7 @@ domain = d["domain"]
 etas = d["etas"]
 Dts = d["Dts"]
 phis = d["phis"]
+#orig_phis = phis
 time_vec = collect(0:Dts[1]:20)
 χs = collect(trans.χ(domain.x_R):Dχs[1]:0)
 nDiscs = length(Dσs)
@@ -26,7 +26,7 @@ nt = length(time_vec)
 Dχ = Dχs[1]
 relevant_χs = 100:201
 relevant_ts = 90:100
-eta_ana = [-1/GRAV*transformedAnalyticPotential_dt(χs[relevant_χs],0*χs[relevant_χs],t,0.3,wave,trans) for t in time_vec]
+eta_ana = [-1/GRAV*transformedAnalyticPotential_dt(χs,0*χs,t,0.3,wave,trans) for t in time_vec] =#
 
 # compute errors L2 in space
 error_phi_L2_time = Vector{Vector{Float64}}(undef,nDiscs-1)
@@ -35,11 +35,12 @@ for disc = 1:nDiscs-1
     error_phi_L2_time[disc] = Vector{Float64}(undef,nt)
     error_eta_L2_time[disc] = Vector{Float64}(undef,nt)
     for t = relevant_ts
-        error_phi_L2_time[disc][t] = computeError(phis[disc][t][relevant_χs],phis[disc+1][t][relevant_χs],Dχ,norm="L2")
+        error_phi_L2_time[disc][t] = sqrt(Dχ^2*sum((phis[disc][t]-phis[disc+1][t]).^2))
+        #error_phi_L2_time[disc][t] = computeError(phis[disc][t],phis[disc+1][t],Dχ,norm="L2")
         if true_eta_ana
-            error_eta_L2_time[disc][t] = computeError(etas[disc][t][relevant_χs],eta_ana[t],Dχ,norm="L2")
+            error_eta_L2_time[disc][t] = computeError(etas[disc][t],eta_ana[t],Dχ,norm="L2")
         else
-            error_eta_L2_time[disc][t] = computeError(etas[disc][t][relevant_χs],etas[disc+1][t][relevant_χs],Dχ,norm="L2")
+            error_eta_L2_time[disc][t] = computeError(etas[disc][t],etas[disc+1][t],Dχ,norm="L2")
         end
     end
 end
@@ -53,11 +54,11 @@ for disc = 1:nDiscs-1
     error_phi_max_time[disc] = Vector{Float64}(undef,nt)
     error_eta_max_time[disc] = Vector{Float64}(undef,nt)
     for t = relevant_ts
-        error_phi_max_time[disc][t] = computeError(phis[disc][t][relevant_χs],phis[disc+1][t][relevant_χs],Dχ,norm="max")
+        error_phi_max_time[disc][t] = computeError(phis[disc][t],phis[disc+1][t],Dχ,norm="max")
         if true_eta_ana
-            error_eta_max_time[disc][t] = computeError(etas[disc][t][relevant_χs],eta_ana[t],Dχ,norm="max")
+            error_eta_max_time[disc][t] = computeError(etas[disc][t],eta_ana[t],Dχ,norm="max")
         else
-            error_eta_max_time[disc][t] = computeError(etas[disc][t][relevant_χs],etas[disc+1][t][relevant_χs],Dχ,norm="max")
+            error_eta_max_time[disc][t] = computeError(etas[disc][t],etas[disc+1][t],Dχ,norm="max")
         end
     end
 end
